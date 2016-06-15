8e5f47054cf54f3a3e3dc60c9a43f257d8eb420d
MatheusZandona <zan_matheus@yahoo.com.br>
2016-06-15 15:53:20 -0300

    Alteração da classe cliente:   - criacao dos campos que estavam faltando para cadastro completo

--- src/main/java/br/univel/model/Cliente.java
+++ src/main/java/br/univel/model/Cliente.java
@@ -1,8 +1,19 @@
 package br.univel.model;
 
 public class Cliente {
-	private int id;   //  matheus
-
+	
+	private int id;   
+	private String nome; 
+	private String endereço; 
+	private int numero;
+	private String complemento; 
+	private String bairro; 
+	private String cidade; 
+	private String estado; 
+	private String cep;
+	private String telefone;
+	private String celular;
+	
 	public int getId() {
 		return id;
 	}
@@ -10,4 +21,85 @@ public class Cliente {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereço() {
+		return endereço;
+	}
+
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	
 }
