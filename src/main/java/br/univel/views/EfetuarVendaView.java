376133adb0e913fea9883f257eb5400d1637cf1f
MatheusZandona <zan_matheus@yahoo.com.br>
2016-06-26 22:37:57 -0300

    asds

--- src/main/java/br/univel/views/EfetuarVendaView.java
+++ src/main/java/br/univel/views/EfetuarVendaView.java
@@ -18,15 +18,19 @@ import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class EfetuarVendaView extends JFrame{
 	private JTextField txtQtdProduto;
-	private JTextField txtprecoProd;
 	private JTextField txtDescricaoProduto;
 	private JTable table_Produtos;
 	private JTextField txtCodCliente;
 	private JTextField txtNomeCliente;
-
+	private JLabel lblNomeCliente;
+	private JComboBox ComboBoxCliente;
 	ClienteDAO clienteDAO = new ClienteDAO();
 	List<Cliente> clienteList = clienteDAO.listAll();
 	ProdutoDAO produtoDAO = new ProdutoDAO();
@@ -64,28 +68,49 @@ public class EfetuarVendaView extends JFrame{
 		gbl_panel_Cliente.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
 		gbl_panel_Cliente.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
 		panel_Cliente.setLayout(gbl_panel_Cliente);
+		
+		JCheckBox checkboxUtilizarCliente = new JCheckBox("Utilizar Cliente");
+		checkboxUtilizarCliente.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(checkboxUtilizarCliente.isSelected()){
+					lblNomeCliente.setEnabled(true);
+					ComboBoxCliente.setEnabled(true);
+				}else{
+					lblNomeCliente.setEnabled(false);
+					ComboBoxCliente.setEnabled(false);
+					ComboBoxCliente.setSelectedIndex(-1);
+				}
+			}
+		});
+		
+		GridBagConstraints gbc_checkboxUtilizarCliente = new GridBagConstraints();
+		gbc_checkboxUtilizarCliente.insets = new Insets(0, 0, 5, 5);
+		gbc_checkboxUtilizarCliente.gridx = 0;
+		gbc_checkboxUtilizarCliente.gridy = 0;
+		panel_Cliente.add(checkboxUtilizarCliente, gbc_checkboxUtilizarCliente);
 
-		JLabel lblNomeCliente = new JLabel("Nome Cliente");
+		lblNomeCliente = new JLabel("Nome Cliente");
 		lblNomeCliente.setHorizontalAlignment(SwingConstants.LEFT);
 
 		GridBagConstraints gbc_lblNomeCliente = new GridBagConstraints();
-		gbc_lblNomeCliente.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNomeCliente.insets = new Insets(0, 0, 0, 5);
 		gbc_lblNomeCliente.gridx = 0;
-		gbc_lblNomeCliente.gridy = 0;
+		gbc_lblNomeCliente.gridy = 1;
 		panel_Cliente.add(lblNomeCliente, gbc_lblNomeCliente);
 
 		DefaultComboBoxModel clienteComboBox = new DefaultComboBoxModel();
 		for (int i = 0; i < clienteList.size(); i++) {
 			clienteComboBox.addElement(clienteList.get(i).getNome());
 		}
-		JComboBox jComboBox = new JComboBox();
-		jComboBox.setModel(clienteComboBox);
+		ComboBoxCliente = new JComboBox();
+		ComboBoxCliente.setModel(clienteComboBox);
 
-		GridBagConstraints gbc_txtNomeCliente = new GridBagConstraints();
-		gbc_txtNomeCliente.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtNomeCliente.gridx = 1;
-		gbc_txtNomeCliente.gridy = 0;
-		panel_Cliente.add(jComboBox, gbc_txtNomeCliente);
+		GridBagConstraints gbc_ComboBoxCliente = new GridBagConstraints();
+		gbc_ComboBoxCliente.fill = GridBagConstraints.HORIZONTAL;
+		gbc_ComboBoxCliente.gridx = 1;
+		gbc_ComboBoxCliente.gridy = 1;
+		panel_Cliente.add(ComboBoxCliente, gbc_ComboBoxCliente);
 
 
 		JPanel panel_Produtos = new JPanel();
@@ -98,15 +123,15 @@ public class EfetuarVendaView extends JFrame{
 		gbc_panel_Produtos.gridy = 2;
 		getContentPane().add(panel_Produtos, gbc_panel_Produtos);
 		GridBagLayout gbl_panel_Produtos = new GridBagLayout();
-		gbl_panel_Produtos.columnWidths = new int[]{72, 87, 0, 0};
+		gbl_panel_Produtos.columnWidths = new int[]{377, 72, 75, 0};
 		gbl_panel_Produtos.rowHeights = new int[]{0, 0, 0, 0};
-		gbl_panel_Produtos.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_Produtos.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_panel_Produtos.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
 		panel_Produtos.setLayout(gbl_panel_Produtos);
 
-		JLabel lblNewLabel_1 = new JLabel("Descrição Produto");
+		JLabel lblNewLabel_1 = new JLabel("Produtos");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_1.gridx = 0;
 		gbc_lblNewLabel_1.gridy = 0;
 		panel_Produtos.add(lblNewLabel_1, gbc_lblNewLabel_1);
@@ -117,13 +142,6 @@ public class EfetuarVendaView extends JFrame{
 		gbc_lblQtd.gridx = 1;
 		gbc_lblQtd.gridy = 0;
 		panel_Produtos.add(lblQtd, gbc_lblQtd);
-		
-		JLabel lblpreco = new JLabel("Preco");
-		GridBagConstraints gbc_lblPreco = new GridBagConstraints();
-		gbc_lblPreco.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPreco.gridx = 2;
-		gbc_lblPreco.gridy = 0;
-		panel_Produtos.add(lblpreco, gbc_lblPreco);
 
 
 		DefaultComboBoxModel produtoComboBox = new DefaultComboBoxModel();
@@ -149,14 +167,17 @@ public class EfetuarVendaView extends JFrame{
 		panel_Produtos.add(txtQtdProduto, gbc_txtQtdProduto);
 		txtQtdProduto.setColumns(5);
 		
-		txtprecoProd = new JTextField();
-		GridBagConstraints gbc_txtprecoProd = new GridBagConstraints();
-		gbc_txtprecoProd.insets = new Insets(0, 0, 5, 5);
-		gbc_txtprecoProd.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtprecoProd.gridx = 2;
-		gbc_txtprecoProd.gridy = 1;
-		panel_Produtos.add(txtprecoProd, gbc_txtprecoProd);
-		txtprecoProd.setColumns(5);
+		JButton btnAddProd = new JButton("add prod");
+		btnAddProd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		GridBagConstraints gbc_btnAddProd = new GridBagConstraints();
+		gbc_btnAddProd.insets = new Insets(0, 0, 5, 0);
+		gbc_btnAddProd.gridx = 2;
+		gbc_btnAddProd.gridy = 1;
+		panel_Produtos.add(btnAddProd, gbc_btnAddProd);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
@@ -193,7 +214,7 @@ public class EfetuarVendaView extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Produto p = new ProdutoDAO().search(produtoList.get(produtojComboBox.getSelectedIndex()).getId());
-				Cliente c = new ClienteDAO().search(clienteList.get(jComboBox.getSelectedIndex()).getId());
+				Cliente c = new ClienteDAO().search(clienteList.get(ComboBoxCliente.getSelectedIndex()).getId());
 
 				NewVendaDAO vendaDAO = new NewVendaDAO();
 				NewVenda venda = new NewVenda();
@@ -213,7 +234,7 @@ public class EfetuarVendaView extends JFrame{
  				itemVenda.setVenda(newVendas);
 				itemVenda.setProduto(p);
 				itemVenda.setQuantidade(new BigDecimal(txtQtdProduto.getText().replaceAll(",", ".")));
-				itemVenda.setPreco(new BigDecimal(txtprecoProd.getText().replaceAll(",", ".")));
+//				itemVenda.setPreco(new BigDecimal(txtprecoProd.getText().replaceAll(",", ".")));
 
 				itemVendaDAO.save(itemVenda);
 
